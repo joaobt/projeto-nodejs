326855fd3387eb9bd1cfbbdf47c412e5deef676b
joao <joao4729@gmail.com>
2022-09-28 09:42:49 -0300

    commit das pastas ejs

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.get('/', function(req, res) {
     res.render('index');
 })
 
-app.get('/', function(req, res) {
+app.get('/sobre', function(req, res) {
     res.render('about');
 })
+
+app.listen(8080);
+console.log('rodadando')
