a679ce107c40ceea4c73720b6a977e7c0203c3e0
joao <joao4729@gmail.com>
2022-09-28 11:04:54 -0300

    colocando a terceira pagina

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ app.get('/sobre', function(req, res) {
     res.render('about');
 })
 
+app.get('/jogo', function(req, res) {
+    res.render('jogo');
+})
+
+
+
 app.listen(8080);
 console.log('rodadando')
